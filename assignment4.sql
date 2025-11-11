2149af39bd31babf7d3df76dd174b151bd401e42
The Vinh Vuong <6311228@edu.vaniercollege.qc.ca>
2025-11-11 11:48:38 -0500

    fixed sql file

--- assignment4.sql
+++ assignment4.sql
@@ -1,3 +1,5 @@
+create Database assignment4;
+use assignment4;
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
@@ -7,54 +9,49 @@
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `assignment4`
+-- Database:  assignment4 
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `department`
+-- Table structure for table  department 
 --
 
-CREATE TABLE `department` (
-  `DepartmentID` int(11) NOT NULL,
-  `DepartmentName` varchar(50) NOT NULL
+CREATE TABLE department (
+   DepartmentID  int(11) NOT NULL,
+   DepartmentName  varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employee`
+-- Table structure for table  employee 
 --
 
-CREATE TABLE `employee` (
-  `EmployeeID` int(11) NOT NULL,
-  `FirstName` varchar(50) NOT NULL,
-  `LastName` varchar(50) NOT NULL,
-  `Salary` int(11) NOT NULL,
-  `DepartmentID` int(11) DEFAULT NULL
+CREATE TABLE  employee  (
+   EmployeeID  int(11) NOT NULL,
+   FirstName  varchar(50) NOT NULL,
+   LastName  varchar(50) NOT NULL,
+   Salary  int(11) NOT NULL,
+   DepartmentID  int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `project`
+-- Table structure for table  project 
 --
 
-CREATE TABLE `project` (
-  `ProjectID` int(11) NOT NULL,
-  `ProjectDescription` varchar(200) NOT NULL
+CREATE TABLE  project  (
+   ProjectID  int(11) NOT NULL,
+   ProjectDescription  varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -62,44 +59,44 @@ CREATE TABLE `project` (
 --
 
 --
--- Indexes for table `department`
+-- Indexes for table  department 
 --
-ALTER TABLE `department`
-  ADD PRIMARY KEY (`DepartmentID`);
+ALTER TABLE  department 
+  ADD PRIMARY KEY ( DepartmentID );
 
 --
--- Indexes for table `employee`
+-- Indexes for table  employee 
 --
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`EmployeeID`);
+ALTER TABLE  employee 
+  ADD PRIMARY KEY ( EmployeeID );
 
 --
--- Indexes for table `project`
+-- Indexes for table  project 
 --
-ALTER TABLE `project`
-  ADD PRIMARY KEY (`ProjectID`);
+ALTER TABLE  project 
+  ADD PRIMARY KEY ( ProjectID );
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `department`
+-- AUTO_INCREMENT for table  department 
 --
-ALTER TABLE `department`
-  MODIFY `DepartmentID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE  department 
+  MODIFY  DepartmentID  int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `employee`
+-- AUTO_INCREMENT for table  employee 
 --
-ALTER TABLE `employee`
-  MODIFY `EmployeeID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE  employee 
+  MODIFY  EmployeeID  int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `project`
+-- AUTO_INCREMENT for table  project 
 --
-ALTER TABLE `project`
-  MODIFY `ProjectID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE  project 
+  MODIFY  ProjectID  int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
